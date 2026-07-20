0ebaa44fd2b7d76929dc3c926dc45e0ec72b3a63
John Probus Sr <105514234+jprobussr@users.noreply.github.com>
2026-07-20 11:50:10 -0500

    Build random color palette generator

--- color-palette-generator/src/App.jsx
+++ color-palette-generator/src/App.jsx
@@ -4,6 +4,36 @@ import './App.css';
 const App = () => {
   const [color, setColor] = useState('#4f46e5');
 
+  const generateRandomColor = () => {
+    const hexDigits = [
+      '0',
+      '1',
+      '2',
+      '3',
+      '4',
+      '5',
+      '6',
+      '7',
+      '8',
+      '9',
+      'A',
+      'B',
+      'C',
+      'D',
+      'E',
+      'F',
+    ];
+    let randomColor = '#';
+
+    for (let i = 0; i < 6; i += 1) {
+      const randomIndex = Math.floor(Math.random() * hexDigits.length);
+
+      randomColor += hexDigits[randomIndex];
+    }
+
+    setColor(randomColor)
+  };
+
   return (
     <main className="app">
       <section className="color-generator">
@@ -13,9 +43,19 @@ const App = () => {
           Generate a random color and view its HEX value.
         </p>
 
-        <div className="color-generator__preview" style={{backgroundColor: color}}>
+        <div
+          className="color-generator__preview"
+          style={{ backgroundColor: color }}
+        >
           <span className="color-generator__value">{color}</span>
         </div>
+
+        <button
+          className="color-generator__button"
+          onClick={generateRandomColor}
+        >
+          Generate New Color
+        </button>
       </section>
     </main>
   );
